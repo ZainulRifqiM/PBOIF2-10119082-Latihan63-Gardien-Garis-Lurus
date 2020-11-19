2740566c99b2920208b8473581ba88236239c440
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-19 19:02:22 +0700

    PBOIF2-10119082-Latihan63-Gardien Garis Lurus

---
+++ src/pboif2/pkg10119082/latihan63/gardien/garis/lurus/GarisLurus.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan63.gardien.garis.lurus;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Gradien Garis Lurus
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
